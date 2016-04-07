e9fcade9850f0c1f821a9adbbb6d1d6c3c27fcc2
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-04-07 10:47:50 +0200

    boletin25_4

---
+++ src/boletin25_4/Boletin25_4.java
@@ -0,0 +1,12 @@
+
+package boletin25_4;
+
+public class Boletin25_4 {
+
+    
+    public static void main(String[] args) {
+        marco254 marco=new marco254();
+        marco.setVisible(true);
+    }
+    
+}
